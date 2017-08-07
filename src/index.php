8081afaa9c390c009ea1eafe65f7aca9b8518766
JimDiGrise <underwater.aviator@gmail.com>
2017-08-07 19:12:50 +0300

    Add accounts class

--- src/index.php
+++ src/index.php
@@ -1,3 +1,22 @@
-<? phpinfo(); 
+<?php
+    
+    require 'core/bot.php';
 
+    class Accounts {
+        private $accounts  = [
+            "eaf3a74c662a387e3b2afafb429f0593a5d3e14475ac0cbc0cd7b031083fcee0135109d553d1fa6a083d0", 
+            "eaf3a74c662a387e3b2afafb429f0593a5d3e14475ac0cbc0cd7b031083fcee0135109d553d1fa6a083d0",
+            "eaf3a74c662a387e3b2afafb429f0593a5d3e14475ac0cbc0cd7b031083fcee0135109d553d1fa6a083d0",
+            "eaf3a74c662a387e3b2afafb429f0593a5d3e14475ac0cbc0cd7b031083fcee0135109d553d1fa6a083d0"
+        ];
+        public function getRandomAccount() {
+            $randomToken = rand(0, count($this->accounts) - 1);
+            return $this->accounts[$randomToken];
+        }
+    }
+    $acc = new Accounts();
+    $newToken = $acc->getRandomAccount();
+    $bot = new Bot();
+    $bot->setToken($newToken);
+    $bot->setLikes();
 ?>
